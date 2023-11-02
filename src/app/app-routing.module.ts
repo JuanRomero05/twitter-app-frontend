915d16d8aa85bed3e5d4c27eb279828d073d0978
Juan <jromero@sysmodelers.com>
2023-11-02 01:20:01 -0400

    Tabs navigation implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tab-inicial',
     pathMatch: 'full'
+  },
+
+  {
+    path: 'tab-inicial',
+    loadChildren: () => import('./page/tab-inicial/tab-inicial.module').then(m => m.TabInicialPageModule)
   },
 ];
 
---
+++ src/app/page/tab-inicial/tab-inicial-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabInicialPage } from './tab-inicial.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabInicialPage,
+    children: [
+      {
+        path: 'homePrincipal',
+        loadChildren: () => import('./../../page/home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'search',
+        loadChildren: () => import('./../../page/search/search.module').then(m => m.SearchPageModule)
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => import('./../../page/notifications/notifications.module').then(m => m.NotificationsPageModule)
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabInicialPageRoutingModule { }
