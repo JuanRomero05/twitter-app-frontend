24ae9e855ddd98bc521ea84b7501ba65ca3c6823
Juan <jromero@sysmodelers.com>
2023-11-04 00:25:49 -0400

    Add tweet component

--- src/app/page/home/home.module.ts
+++ src/app/page/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { TweetComponent } from 'src/app/components/tweet/tweet.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,9 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, TweetComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  segment: String = 'home';
 
-  constructor() { }
+  tweets = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://devdactic.fra1.digitaloceanspaces.com/twitter-ui/tweets.json').subscribe((data: any) => {
+      console.log('tweets: ', data.tweets);
+      this.tweets = data.tweets;
+    })
   }
 
 }
