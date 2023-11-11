7a6f793d152d405896d56e70ba5bd6a944eb9c87
Juan <jromero@sysmodelers.com>
2023-11-11 01:34:35 -0400

    Changes in UI/UX of profile page

--- src/app/page/profile/profile.page.ts
+++ src/app/page/profile/profile.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { OverlayEventDetail } from '@ionic/core/components';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,13 +9,37 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+
+
+  constructor(private http: HttpClient) {
+    this.modal = null as any
+  }
+
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
   segment: String = 'posts';
 
   tweets = [];
 
   showNewTweet = false;
 
-  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get('https://devdactic.fra1.digitaloceanspaces.com/twitter-ui/tweets.json').subscribe((data: any) => {
