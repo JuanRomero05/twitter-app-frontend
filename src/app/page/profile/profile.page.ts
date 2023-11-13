f747f2830d219f0b84f23f25582d45b3058b3513
Juan <jromero@sysmodelers.com>
2023-11-12 21:30:13 -0400

    Add form in edit profile

--- src/app/page/profile/profile.page.ts
+++ src/app/page/profile/profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { IonModal } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -10,18 +11,37 @@ import { AlertController } from '@ionic/angular';
 })
 export class ProfilePage implements OnInit {
 
+  editProfileForm: FormGroup;
+
   constructor(
     private http: HttpClient,
-    public alertController: AlertController
+    public alertController: AlertController,
+    public fb: FormBuilder
   ) {
     this.modalFollowing = null as any
     this.modalFollowers = null as any
     this.modalEditProfile = null as any
+
+    this.editProfileForm = this.fb.group({
+      'firstName': new FormControl,
+      'lastName': new FormControl,
+      'bio': new FormControl,
+      'password': new FormControl,
+      'repeatPassword': new FormControl,
+    }, { validator: this.pwMatchValidator })
   }
 
   @ViewChild(IonModal) modalFollowing: IonModal;
   @ViewChild('followersModal') modalFollowers: IonModal;
   @ViewChild('editProfileModal') modalEditProfile: IonModal;
+
+  @ViewChild('passwordInput') passwordInput: any;
+  @ViewChild('repeatPasswordInput') repeatPasswordInput: any;
+
+  showPassword: boolean = false;
+  showRepeatPassword: boolean = false;
+  eyeIcon: string = 'eye-outline';
+  eyeIconRepeat: string = 'eye-outline';
 
   segment: String = 'posts';
 
@@ -34,6 +54,32 @@ export class ProfilePage implements OnInit {
       console.log('tweets: ', data.tweets);
       this.tweets = data.tweets;
     })
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+    this.passwordInput.type = this.showPassword ? 'text' : 'password';
+    this.eyeIcon = this.showPassword ? 'eye-off' : 'eye-outline';
+
+  }
+
+  toggleRepeatPasswordVisibility() {
+    this.showRepeatPassword = !this.showRepeatPassword;
+    this.repeatPasswordInput.type = this.showRepeatPassword ? 'text' : 'password';
+    this.eyeIconRepeat = this.showRepeatPassword ? 'eye-off' : 'eye-outline';
+  }
+
+  pwMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const repeatPassword = control.get('repeatPassword')?.value;
+
+    if (password !== repeatPassword) {
+      control.get('repeatPassword')?.setErrors({ 'passwordMismatch': true });
+
+    } else {
+      control.get('repeatPassword')?.setErrors(null);
+
+    }
   }
 
   cancelModalFollowing() {
@@ -75,6 +121,10 @@ export class ProfilePage implements OnInit {
     const alert = await this.createAlert('Log Out?', 'Are you sure you want to log out?')
     await alert.present();
     return;
+  }
+
+  async saveEditData() {
+    console.log('giovanni es gay');
   }
 
 }
