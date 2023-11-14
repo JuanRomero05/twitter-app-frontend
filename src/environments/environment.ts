c299ce1085d8d84f7d594d8ab50a96f999e582a3
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-14 12:46:46 -0400

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //api: 'https://twitter-api-awdc.onrender.com/api/'
-  api: 'http://localhost:4000/api/'
+  api: 'https://twitter-api-awdc.onrender.com/api/'
+  //api: 'http://localhost:4000/api/'
 };
 
 /*
